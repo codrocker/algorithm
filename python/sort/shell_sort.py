b0088c337e085c1738f6c5e398efb1d7116c1afc
andy <tmacwsw@126.com>
2016-09-21 22:59:50 +0800

    shell sory by python 30 percent completed

--- python/sort/shell_sort.py
+++ python/sort/shell_sort.py
@@ -10,17 +10,7 @@ for x in range(1, data_len + 1):
     data.append(random.randint(1, 100))
 
 def shell_sort(data, start, end):
-    index = start + 1
-    while index <= end:
-        back = index
-        while back > start:
-            if data[back] < data[back - 1]:
-                data[back], data[back - 1] = data[back -1], data[back]
-                back -= 1
-            else:
-                break
-        index += 1
+    print end - start
 
-print data
 shell_sort(data, 0, data_len - 1)
 print data
